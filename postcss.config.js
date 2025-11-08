4fe0ae4315a25040a2f60fa65931f70596dac06c
raph bmw <theuitheuil41150@gmail.com>
2025-11-09 05:18:11 +0800

    prugr

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,7 @@ export default {
     autoprefixer(),
     ...(isProd
       ? [
-          purgecss({
+          new purgecss({
             content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
