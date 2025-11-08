441f826ea73a3085a1c0f2251609a5c73715e351
raph bmw <theuitheuil41150@gmail.com>
2025-11-09 05:20:04 +0800

    okkk

--- postcss.config.js
+++ postcss.config.js
@@ -4,13 +4,15 @@ import purgecss from "@fullhuman/postcss-purgecss";
 
 const isProd = process.env.NODE_ENV === "production";
 
+const purge = purgecss.default || purgecss; // ✅ pour compat ESM/CommonJS
+
 export default {
   plugins: [
     tailwindcss(),
     autoprefixer(),
     ...(isProd
       ? [
-          new purgecss({
+          purge({
             content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
